2dd3af88f34dcc60b0ef698cd2e76d28cae96c18
felix-laumann <laumannfelix@gmail.com>
2018-05-29 11:26:31 +0200

    weight decay, extract

--- extraction/extract_acc.py
+++ extraction/extract_acc.py
@@ -5,22 +5,21 @@ import matplotlib.pyplot as plt
 plt.style.use("seaborn")
 import re
 import numpy as np
+plt.rc('font', family='serif', size=16)
+plt.rcParams.update({'xtick.labelsize': 16, 'ytick.labelsize': 16, 'axes.labelsize': 16})
 
 # change for given number of tasks
-task = "LeNet"
+task = "1"
 
 
 def load_data():
     os.chdir("/home/felix/Dropbox/publications/Bayesian_CNN_MCVI/results/")
     with open("diagnostics_{}.txt".format(task), 'r') as file:
         acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
-    #acc = list(map(lambda x: x.split(" ")[-1], re.findall(r"(acc: \d.\d+)", file)))
-
-    #print(re.findall(r"(acc: \d.\d+)", file))
     print(acc)
 
-    train = acc[0::3]
-    valid = acc[1::3]
+    train = acc[0::2]
+    valid = acc[1::2]
     return np.array(train).astype(np.float32), np.array(valid).astype(np.float32)
 
 
@@ -31,15 +30,13 @@ train, valid = load_data()
 
 print(valid)
 
-plt.plot(valid, "--", label=r"Validation, prior: $\mathcal{U}(a, b)$", color="#34d536")
-#plt.plot(i, valid, "--", label=r"Validation, prior: $q(w \mid \theta)$", color="#d534d3")
+plt.plot(valid, "--", label=r"Validation, prior: $U(a, b)$", color='maroon')
 
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
 x_ticks = range(len(valid))
 plt.xticks(x_ticks[9::10], map(lambda x: x+1, x_ticks[9::10]))
-#for label in f.ax.xaxis.get_ticklabels()[::5]:
-#    label.set_visible(True)
+
 f.suptitle("Accuracy after training for 100 epochs")
 plt.legend()
 
