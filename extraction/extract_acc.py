48fe157efa227e1ca1dbecd7583c8e00fe31d0f5
felix-laumann <laumannfelix@gmail.com>
2018-05-30 22:40:32 +0200

    see changes

--- extraction/extract_acc.py
+++ extraction/extract_acc.py
@@ -18,13 +18,12 @@ def load_data():
         acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
     print(acc)
 
-    train = acc[0::2]
-    valid = acc[1::2]
+    train = acc[1::2]
+    valid = acc[0::2]
     return np.array(train).astype(np.float32), np.array(valid).astype(np.float32)
 
 
 f = plt.figure(figsize=(10, 8))
-name_ext = task
 
 train, valid = load_data()
 
@@ -37,7 +36,7 @@ plt.ylabel("Accuracy")
 x_ticks = range(len(valid))
 plt.xticks(x_ticks[19::20], map(lambda x: x+1, x_ticks[19::20]))
 
-f.suptitle("Accuracy after training for 200 epochs")
+f.suptitle("Accuracy after training for 100 epochs")
 plt.legend()
 
 plt.savefig("results_{}.png".format(task))
